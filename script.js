1480a34ee9cc7495e7d864f49be9d1c4dfad5f37
Betina9 <ob042025@gmail.com>
2025-07-31 11:15:10 +0200

    lagt til funksjon på knappen

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function increment() {
   console.log("The button was clicked");
-}
+
+let count = 0;
+
+const countEl = document.getElementById("count-el");
+const incrementBtn = document.getElementById("Increment-btn");
+const resetBtn = document.getElementById("Reset-btn");
+
+incrementBtn.addEventListener("click", function () {
+  count += 1;
+  countEl.textContent = count;
+});
+
+resetBtn.addEventListener("click", function () {
+  count = 0;
+  countEl.textContent = count;
+});
